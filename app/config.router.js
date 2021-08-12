a24c623f1fd5cf5cde4132a8ac989ab2ad18d110
Quentin784 <quentin.engelbrecht@lamacs.co.za>
2021-08-12 13:55:22 +0200

    initial project setup and commit

---
+++ app/config.router.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Config for the router
+ */
+angular.module('app')
+    .run(
+        ['$rootScope', '$state', '$stateParams',
+            function ($rootScope, $state, $stateParams) {
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+            }
+        ]
+    )
+    .config(
+        ['$stateProvider', '$urlRouterProvider',
+            function ($stateProvider, $urlRouterProvider) {
+
+                var layout = "index.html";
+
+                $urlRouterProvider.otherwise('/app/home');
+
+                $stateProvider
+                    .state('app', {
+                        abstract: true,
+                        url: '/app',
+                        templateUrl: layout
+                    });                
+            }
+        ]
+    );
